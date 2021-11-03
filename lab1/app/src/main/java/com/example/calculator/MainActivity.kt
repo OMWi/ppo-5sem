7ab8aaa2cd92f256ec03738629c89ec6cb6bf643
OMWi <bsuiromwi@gmail.com>
2021-11-03 23:20:07 +0300

    add funcs

--- lab1/app/src/main/java/com/example/calculator/MainActivity.kt
+++ lab1/app/src/main/java/com/example/calculator/MainActivity.kt
@@ -4,28 +4,46 @@ import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
 import android.view.View
 import android.widget.Button
+import android.widget.ScrollView
 import android.widget.TextView
-import androidx.core.text.isDigitsOnly
+import androidx.core.widget.doAfterTextChanged
 import net.objecthunter.exp4j.ExpressionBuilder
 import net.objecthunter.exp4j.operator.Operator
 
 
-
-
 class MainActivity : AppCompatActivity() {
-    private lateinit var inputText : TextView
-    private lateinit var resultText : TextView
+    private lateinit var inputText: TextView
+    private lateinit var resultText: TextView
+    private lateinit var scrollView: ScrollView
+    private lateinit var sinButton: Button
+    private lateinit var cosButton: Button
+    private lateinit var tanButton: Button
+    private lateinit var rootButton: Button
+
     private var inputs = mutableListOf<String>("0")
+    private val funcs = arrayOf("sin(", "cos(", "tan(", "sinh(", "cosh(", "tanh(", "sqrt(",
+        "cbrt(", "log(", "log10(")
     private var str = "0"
+    private var bracketCount = 0
+    private var isAltButtons = false
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
         inputText = findViewById(R.id.textInput)
         resultText = findViewById(R.id.textResult)
+        scrollView = findViewById(R.id.scrollView)
+        sinButton = findViewById(R.id.buttonSin)
+        cosButton = findViewById(R.id.buttonCos)
+        tanButton = findViewById(R.id.buttonTan)
+        rootButton = findViewById(R.id.buttonRoot)
+
+        inputText.doAfterTextChanged { _ ->
+            scrollView.fullScroll(View.FOCUS_DOWN)
+        }
     }
 
-    var factorial: Operator = object : Operator("!", 1, true, PRECEDENCE_POWER + 1) {
+    private var factorial: Operator = object : Operator("!", 1, true, PRECEDENCE_POWER + 1) {
         override fun apply(vararg args: Double): Double {
             val arg = args[0].toInt()
             require(arg.toDouble() == args[0]) { "Operand for factorial has to be an integer" }
@@ -47,6 +65,11 @@ class MainActivity : AppCompatActivity() {
         var buttonText = v.text.toString()
         str += buttonText
         inputs.add(buttonText)
+        if (buttonText == "(")
+            bracketCount++
+        if (buttonText == ")")
+            bracketCount--
+        resultText.text = "brackets = $bracketCount"
         inputText.text = str
     }
 
@@ -56,6 +79,8 @@ class MainActivity : AppCompatActivity() {
         inputs.add("0")
         inputText.text = "0"
         resultText.text = ""
+        bracketCount = 0
+        resultText.text = "brackets = $bracketCount"
     }
 
     fun onClickBackspace(v : View) {
@@ -67,15 +92,38 @@ class MainActivity : AppCompatActivity() {
         }
         var lastInput = inputs.last()
         inputs.removeLast()
-        if (lastInput == "log10") {
-            str = str.dropLast(5)
+        str = when (lastInput) {
+            "log10(" -> {
+                bracketCount--
+                str.dropLast(resources.getString(R.string._lg).length + 1)
+            }
+            "log(" -> {
+                bracketCount--
+                str.dropLast(resources.getString(R.string._ln).length + 1)
+            }
+            "cbrt(" -> {
+                bracketCount--
+                str.dropLast(resources.getString(R.string._cbrt).length + 1)
+            }
+            "sqrt(" -> {
+                bracketCount--
+                str.dropLast(resources.getString(R.string._sqrt).length + 1)
+            }
+            "(" -> {
+                bracketCount--
+                str.dropLast(1)
+            }
+            ")" -> {
+                bracketCount++
+                str.dropLast(1)
+            }
+            else -> {
+                if (funcs.contains(lastInput))
+                    bracketCount--
+                str.dropLast(lastInput.length)
+            }
         }
-        else if (lastInput == "log") {
-            str = str.dropLast(3)
-        }
-        else {
-            str = str.dropLast(lastInput.length)
-        }
+        resultText.text = "brackets = $bracketCount; str=" + str
         if (str.isEmpty()) {
             str = "0"
             inputs.add("0")
@@ -111,37 +159,79 @@ class MainActivity : AppCompatActivity() {
             str = ""
         }
         var buttonText = v.text.toString()
-        if (buttonText == "lg") {
-            inputs.add("log10(")
+        when (buttonText) {
+            resources.getString(R.string._lg) -> {
+                inputs.add("log10(")
+                str += "${resources.getString(R.string._lg)}("
+            }
+            resources.getString(R.string._ln) -> {
+                inputs.add("log(")
+                str += "${resources.getString(R.string._ln)}("
+            }
+            resources.getString(R.string._sqrt) -> {
+                inputs.add("sqrt(")
+                str += "${resources.getString(R.string._sqrt)}("
+            }
+            resources.getString(R.string._cbrt) -> {
+                inputs.add("cbrt(")
+                str += "${resources.getString(R.string._cbrt)}("
+            }
+            else -> {
+                inputs.add("$buttonText(")
+                str += "$buttonText("
+            }
         }
-        else if (buttonText == "ln") {
-            inputs.add("log(")
+        inputText.text = str
+        bracketCount++
+        resultText.text = "brackets = $bracketCount"
+    }
+
+    fun onClick2nd(v : View) {
+        v as Button
+        if (isAltButtons) {
+            sinButton.text = resources.getString(R.string._sin)
+            cosButton.text = resources.getString(R.string._cos)
+            tanButton.text = resources.getString(R.string._tan)
+            rootButton.text = resources.getString(R.string._sqrt)
         }
         else {
-            inputs.add("$buttonText(")
+            sinButton.text = resources.getString(R.string._sinh)
+            cosButton.text = resources.getString(R.string._cosh)
+            tanButton.text = resources.getString(R.string._tanh)
+            rootButton.text = resources.getString(R.string._cbrt)
         }
-        str += "$buttonText("
-        inputText.text = str
+        isAltButtons = !isAltButtons
     }
 
     fun onClickEqual(v : View) {
         try {
+            while (bracketCount > 0) {
+                inputs.add(")")
+                str = "$str)"
+                bracketCount--
+            }
+            resultText.text = "brackets = $bracketCount"
+            inputText.text = str
             var expression = ""
             for (elem in inputs) {
                 expression += elem
             }
             val ex = ExpressionBuilder(expression).operator(factorial).build()
             var res = ex.evaluate()
-            var resString = "%.15f".format(res)
-            var pointIndex = resString.findLast { it == ',' }
-            if (pointIndex != null) {
+            var resString = "%.8f".format(res)
+            var pointIndex = resString.indexOfLast { it == ','}
+            if (pointIndex != -1) {
                 resString = resString.dropLastWhile { it == '0'}
-                resString = resString.dropLast(1)
+                if (resString.last() == ',') {
+                    resString = resString.dropLast(1)
+                    if (resString == "-0") resString = "0"
+                }
             }
             resultText.text = resString
         }
         catch (e : Exception) {
-            resultText.text = "error:${e.message}"
+            val errMsg : String = resources.getString(R.string._errMsg)
+            resultText.text = errMsg
         }
     }
 }
