37e92e309d3d0828d3bf799a9081fe6c8d29a732
OMWi <bsuiromwi@gmail.com>
2021-11-16 18:54:19 +0300

    init lab3

---
+++ lab3/app/src/main/java/com/example/player/MainActivity.kt
@@ -0,0 +1,19 @@
+package com.example.player
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import androidx.viewpager.widget.ViewPager
+import com.google.android.material.tabs.TabLayout
+
+class MainActivity : AppCompatActivity() {
+    private lateinit var viewPager: ViewPager
+    private lateinit var tabLayout: TabLayout
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        viewPager = findViewById(R.id.view_pager)
+        tabLayout = findViewById(R.id.tab_layout)
+    }
+}
