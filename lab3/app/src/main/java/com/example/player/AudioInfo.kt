1a59f3ad72f1c0a0b7d935f3987c7017343fe143
OMWi <bsuiromwi@gmail.com>
2021-11-20 21:22:18 +0300

    removed lab3

--- lab3/app/src/main/java/com/example/player/AudioInfo.kt
+++
@@ -1,8 +0,0 @@
-package com.example.player
-
-import java.io.Serializable
-
-class AudioInfo(var data: String, var title: String,
-                var album: String, var artist: String) : Serializable {
-
-}
